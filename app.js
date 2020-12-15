9766fa754e250513485868da6b7ade655e102959
Leon <leonrubner2809@gmail.com>
2020-12-15 18:35:11 +0100

    added a lot today, adding, updating posts

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
+const path = require('path');
 const express = require('express');
 const feedRoutes = require('./routes/feed');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${new Date().toISOString()}-${file.originalname}`);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
 //app.use(bodyParser.urlencoded());
 app.use(bodyParser.json()); // application/json
+app.use(multer({
+    storage: fileStorage,
+    fileFilter: fileFilter
+})
+    .single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -15,5 +45,22 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res
+        .status(status)
+        .json({
+            message: message
+        })
+});
 
-app.listen(8080);
+mongoose
+    .connect('mongodb://localhost:27017/messages')
+    .then(() => {
+        app.listen(8080);
+    })
+    .catch(err => {
+        console.log(err);
+    });
